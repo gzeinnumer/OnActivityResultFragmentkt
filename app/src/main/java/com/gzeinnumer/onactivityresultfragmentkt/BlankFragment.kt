d12ddcc366a14eeffa2d43ea143c9454651da247
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-04-25 00:57:01 +0700

    done

--- app/src/main/java/com/gzeinnumer/onactivityresultfragmentkt/BlankFragment.kt
+++ app/src/main/java/com/gzeinnumer/onactivityresultfragmentkt/BlankFragment.kt
@@ -35,7 +35,6 @@ class BlankFragment : Fragment() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 1) {
             if (resultCode == Activity.RESULT_OK) {
                 val bitmap: Bitmap = data?.extras?.get("data") as Bitmap
